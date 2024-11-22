5e83c032f7fa32e01a7713652ffa6a73f3302c56
Natalia Sugrobova <natalyasugrobova@gmail.com>
2024-11-22 14:06:08 +0100

    connecting front and back for Connexion and Inscription

--- src/components/Connexion/index.jsx
+++ src/components/Connexion/index.jsx
@@ -2,40 +2,79 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import axios from 'axios';
 
 import './style.css';
 import Navigation from '../Navigation/Nav';
 import Footer from '../Footer';
 
-const Connexion = () => (
-<>
-  <Navigation />
-  <div className="form-container">
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail"></Form.Group>
-        <h1>Se Connecter</h1>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Entrer votre email" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Mot de passe</Form.Label>
-          <Form.Control type="password" placeholder="Entrer votre mot de passe" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Rester connecté-e" />
-        </Form.Group>
-        <div className="connexionContainer">
-            <Button className="buttonForm" type="submit">Connexion</Button>
-          <Form.Label>(Mot de passe oublié ?)</Form.Label>
-        </div>
-        <div className="creationContainer">
-          <p id="textConnection">Pas de compte ?</p>
-          <Button className="buttonForm"><Link to="/inscription">Créer un compte</Link></Button>
-        </div>
-      </Form>
-    </div>
-    <Footer />
-  </>
-);
+const Connexion = () => {
+  // Creating hooks for managinf the state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Creating a link to the endpoint for logging in using the url from the backend
+  const apiUrlConnect = "http://localhost:8080/public/login";
+
+  const login = async (e) => {
+    e.preventDefault();
+
+    const ExistingUser = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      await axios.post(apiUrlConnect, ExistingUser);
+      console.log("User found.");
+
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.log("The user has not beed found", error);
+    }
+  };
+
+  return (
+  <>
+    <Navigation />
+    <div className="form-container">
+        <Form onSubmit={login}>
+          <Form.Group className="mb-3" controlId="formBasicEmail"></Form.Group>
+          <h1>Se Connecter</h1>
+          <Form.Group className="mb-3" controlId="formBasicEmail" >
+            <Form.Label>Email</Form.Label>
+            <Form.Control 
+              type="email" 
+              placeholder="Entrer votre email" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Mot de passe</Form.Label>
+            <Form.Control 
+              type="password" 
+              placeholder="Entrer votre mot de passe"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Rester connecté-e" />
+          </Form.Group>
+          <div className="connexionContainer">
+              <Button className="buttonForm" type="submit">Connexion</Button>
+            <Form.Label>(Mot de passe oublié ?)</Form.Label>
+          </div>
+          <div className="creationContainer">
+            <p id="textConnection">Pas de compte ?</p>
+            <Button className="buttonForm"><Link to="/inscription">Créer un compte</Link></Button>
+          </div>
+        </Form>
+      </div>
+      <Footer />
+    </>
+  )
+};
 export default Connexion;
--- src/components/Inscription/index.jsx
+++ src/components/Inscription/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -7,44 +8,124 @@ import '../Connexion/style.css';
 import Navigation from '../Navigation/Nav';
 import Footer from '../Footer';
 
-const Inscription = () => (
-<>
-  <Navigation />
-  <div className="form-container">
-      <Form>
-        <h1>Créer un compte</h1>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Entrer votre email" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Mot de passe</Form.Label>
-          <Form.Control type="password" placeholder="Entrer votre mot de passe" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicUsername">
-          <Form.Label>Pseudo</Form.Label>
-          <Form.Control type="text" placeholder="Entrer votre pseudo" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicRadio">
-          <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
-            <label class="form-check-label" for="inlineRadio1">Femme</label>
+const Inscription = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [gender, setGender] = useState("");
+
+  const apiUrlSubscribe = "http://localhost:8080/public/subscribe";
+
+  const addUser = async (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      email: email,
+      password: password,
+      username: username,
+      gender: gender,
+    };
+
+    try {
+      await axios.post(apiUrlSubscribe, newUser);
+      console.log("User added.");
+
+      setEmail("");
+      setPassword("");
+      setUsername("");
+      setGender("");
+    } catch (error) {
+      console.log("Error while adding a user", error);
+    }
+  };
+
+  return (
+    <>
+      <Navigation />
+      <div className="form-container">
+        <Form onSubmit={addUser}>
+          <h1>Créer un compte</h1>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Entrer votre email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Mot de passe</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Entrer votre mot de passe"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Pseudo</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Entrer votre pseudo"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicRadio">
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio1"
+                value="Femme"
+                onChange={(e) => setGender(e.target.value)}
+              />
+              <label className="form-check-label" htmlFor="inlineRadio1">
+                Femme
+              </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio2"
+                value="Homme"
+                onChange={(e) => setGender(e.target.value)}
+              />
+              <label className="form-check-label" htmlFor="inlineRadio2">
+                Homme
+              </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio3"
+                value="Ne se prononce pas"
+                onChange={(e) => setGender(e.target.value)}
+              />
+              <label className="form-check-label" htmlFor="inlineRadio3">
+                Ne se prononce pas
+              </label>
+            </div>
+          </Form.Group>
+          <div className="creationContainer">
+            <Button className="buttonForm" type="submit">
+              Créer un compte
+            </Button>
           </div>
-          <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
-            <label class="form-check-label" for="inlineRadio2">Homme</label>
-          </div>
-          <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
-            <label class="form-check-label" for="inlineRadio3">Ne se prononce pas</label>
-          </div>
-        </Form.Group>
-        <div className="creationContainer">
-          <Button className="buttonForm"><Link to="/">Créer un compte</Link></Button>
-        </div>
-      </Form>
-    </div>
-    <Footer />
-  </>
-);
+        </Form>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
 export default Inscription;
