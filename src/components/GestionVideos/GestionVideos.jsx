594f26c95ed6aa55aba5436776e95d5f87f72308
Lheti <quintero_a@hotmail.fr>
2024-12-18 14:24:26 +0100

    Deuxième essai

---
+++ src/components/GestionVideos/GestionVideos.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import { Container } from 'react-bootstrap';
+
+import './gestionvideos.css';
+
+const GestionVideos = () => (
+  <>
+    <Container className="corpsGestionVideos">
+      <div className="h2etBouton">
+        <h2>Gérer mes vidéos</h2>
+        <button className="ajoutervideo">Ajouter une vidéo</button>
+      </div>
+      
+      <div className="grillevideos">
+      <Container className="containervideo">
+        <iframe width="260" height="145" src="https://www.youtube.com/embed/3s0bB1gbMEU?si=Hw3kzhs7CjRKm-5I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="detailsvideos">
+          <div className="infosvideo">
+          <span className="titrevideo">Nom de la vidéo</span>
+          <span className="datevideo">Date d'ajout</span>
+          </div>
+          <button>Modifier</button>
+        </div>
+        </Container>
+        <Container className="containervideo">
+        <iframe width="260" height="145" src="https://www.youtube.com/embed/3s0bB1gbMEU?si=Hw3kzhs7CjRKm-5I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="detailsvideos">
+          <div className="infosvideo">
+          <span className="titrevideo">Nom de la vidéo</span>
+          <span className="datevideo">Date d'ajout</span>
+          </div>
+          <button>Modifier</button>
+        </div>
+        </Container>
+        <Container className="containervideo">
+        <iframe width="260" height="145" src="https://www.youtube.com/embed/3s0bB1gbMEU?si=Hw3kzhs7CjRKm-5I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="detailsvideos">
+          <div className="infosvideo">
+          <span className="titrevideo">Nom de la vidéo</span>
+          <span className="datevideo">Date d'ajout</span>
+          </div>
+          <button>Modifier</button>
+        </div>
+        </Container>
+        <Container className="containervideo">
+        <iframe width="260" height="145" src="https://www.youtube.com/embed/3s0bB1gbMEU?si=Hw3kzhs7CjRKm-5I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="detailsvideos">
+          <div className="infosvideo">
+          <span className="titrevideo">Nom de la vidéo</span>
+          <span className="datevideo">Date d'ajout</span>
+          </div>
+          <button>Modifier</button>
+        </div>
+        </Container>
+        <Container className="containervideo">
+        <iframe width="260" height="145" src="https://www.youtube.com/embed/3s0bB1gbMEU?si=Hw3kzhs7CjRKm-5I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="detailsvideos">
+          <div className="infosvideo">
+          <span className="titrevideo">Nom de la vidéo</span>
+          <span className="datevideo">Date d'ajout</span>
+          </div>
+          <button>Modifier</button>
+        </div>
+        </Container>
+        <Container className="containervideo">
+        <iframe width="260" height="145" src="https://www.youtube.com/embed/3s0bB1gbMEU?si=Hw3kzhs7CjRKm-5I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="detailsvideos">
+          <div className="infosvideo">
+          <span className="titrevideo">Nom de la vidéo</span>
+          <span className="datevideo">Date d'ajout</span>
+          </div>
+          <button>Modifier</button>
+        </div>
+        </Container>
+        <Container className="containervideo">
+        <iframe width="260" height="145" src="https://www.youtube.com/embed/3s0bB1gbMEU?si=Hw3kzhs7CjRKm-5I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="detailsvideos">
+          <div className="infosvideo">
+          <span className="titrevideo">Nom de la vidéo</span>
+          <span className="datevideo">Date d'ajout</span>
+          </div>
+          <button>Modifier</button>
+        </div>
+        </Container>
+        <Container className="containervideo">
+        <iframe width="260" height="145" src="https://www.youtube.com/embed/3s0bB1gbMEU?si=Hw3kzhs7CjRKm-5I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="detailsvideos">
+          <div className="infosvideo">
+          <span className="titrevideo">Nom de la vidéo</span>
+          <span className="datevideo">Date d'ajout</span>
+          </div>
+          <button>Modifier</button>
+        </div>
+        </Container>
+        <Container className="containervideo">
+        <iframe width="260" height="145" src="https://www.youtube.com/embed/3s0bB1gbMEU?si=Hw3kzhs7CjRKm-5I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="detailsvideos">
+          <div className="infosvideo">
+          <span className="titrevideo">Nom de la vidéo</span>
+          <span className="datevideo">Date d'ajout</span>
+          </div>
+          <button>Modifier</button>
+        </div>
+        </Container>
+        <Container className="containervideo">
+        <iframe width="260" height="145" src="https://www.youtube.com/embed/3s0bB1gbMEU?si=Hw3kzhs7CjRKm-5I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="detailsvideos">
+          <div className="infosvideo">
+          <span className="titrevideo">Nom de la vidéo</span>
+          <span className="datevideo">Date d'ajout</span>
+          </div>
+          <button>Modifier</button>
+        </div>
+        </Container>
+      </div>
+    </Container>
+  </>
+);
+
+export default GestionVideos;
