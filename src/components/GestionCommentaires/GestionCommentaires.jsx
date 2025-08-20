c216fa84f1920e7f0817edf6a3d6d3ad5bf374d0
Sonia Bolivard <biloums@gmail.com>
2025-08-20 13:58:35 +0200

    page GestionCommentaire finie

--- src/components/GestionCommentaires/GestionCommentaires.jsx
+++ src/components/GestionCommentaires/GestionCommentaires.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import NavAdmin from "../NavAdmin/NavAdmin";
 import FooterAdmin from "../FooterAdmin/FooterAdmin";
+import "./GestionCommentaires.css"; 
 
 const GestionCommentaires = () => { 
   return (
-    <div>
+    <div className="conteneur-comm">
       <NavAdmin />
-      <h1>Gestion des Commentaires</h1>
-      <p>Cette section permettra de gérer les commentaires des membres.</p>
+      <main className="contenu-comm">
+        <h1 className="titre-commentaires">Gestion des Commentaires</h1>
+        <p className="texte-comm">Cette section permettra de gérer les commentaires des membres.</p>
+      </main>
       <FooterAdmin />
     </div>
   );
