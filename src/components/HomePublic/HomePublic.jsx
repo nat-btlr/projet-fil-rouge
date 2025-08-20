efadb997c0891cb34fd57a4f1d3d6e5332788e62
Sonia Bolivard <biloums@gmail.com>
2025-08-20 20:42:15 +0200

    page home terminée mais je n'arrive pas à retirer le scroll

--- src/components/HomePublic/HomePublic.jsx
+++ src/components/HomePublic/HomePublic.jsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
 import Navigation from '../Navigation/Nav';
-import photoAcceuil from '../Images/pexels_12.jpg';
+import photoAccueil from '../Images/pexels_12.jpg';
 import Footer from '../Footer';
 
-import './homepublic.css';
+import './Homepublic.css';
 
 const Home = () => (
   <>
     <div id="fond">
     <Navigation />
     <div className="welcomeWords">
-      <img src={photoAcceuil} alt="Accueil" id="pexels"/>
+      <img src={photoAccueil} alt="Accueil" id="pexels"/>
       <h1 className="main-h1">Bienvenue sur la plateforme Futures Mamans !</h1>
       <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       <Button className="welcomeButton" variant="outline-primary"><Link to="/connexion">Se connecter</Link></Button>{' '}
