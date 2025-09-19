49a847cb22ad1f2e27c83f4788ff547fc8199d4c
Natalia Sugrobova <natalyasugrobova@gmail.com>
2025-09-19 19:30:19 +0200

    Fixing deployment 3

--- src/components/FooterAdmin/footeradmin.jsx
+++ src/components/FooterAdmin/footeradmin.jsx
@@ -4,7 +4,7 @@ import './Footeradmin.css';
 const FooterAdmin = () => (
   <>
     <div id="monFooterAdmin">
-      <p className='copyright'>Projet Fil Rouge du Groupe 3 de la Promo Gladys des Descodeuses.</p>
+      <p className='copyright'>Projet Fil Rouge du Groupe 3 de la Promo Gladys des Descodeuses</p>
     </div>
   </>
 );
