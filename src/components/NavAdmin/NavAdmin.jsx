1fe1093c12b7327c049abeaca3937b186369c726
Sonia Bolivard <biloums@gmail.com>
2025-09-23 10:33:23 +0200

    lien de deconnexion nav admin

--- src/components/NavAdmin/NavAdmin.jsx
+++ src/components/NavAdmin/NavAdmin.jsx
@@ -6,23 +6,36 @@ import './NavAdmin.css';
 import logo from '../Images/logo.png'; 
 
 const NavAdmin = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [admin, setAdmin] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const user = localStorage.getItem("user");
-    setIsLoggedIn(!!user); 
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      try {
+        setAdmin(JSON.parse(storedUser)); // si "user" est un objet
+      } catch (e) {
+        setAdmin(storedUser); // si "user" est juste une string
+      }
+    }
   }, []);
 
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    setAdmin(null);
+    navigate("/");
+  };
+
   const handleLinkClick = (e, to) => {
-    if (!isLoggedIn && (to.includes('video') || to.includes('preparation'))) {
+    if (!admin && (to.includes('video') || to.includes('preparation'))) {
       e.preventDefault();
       navigate('/connexion');
     }
   };
 
   return (
-    <Navbar className="navbar-admin" expand="lg" >
+    <Navbar className="navbar-admin" expand="lg">
       <Container fluid>
         <Navbar.Brand className='navLogoAdmin'>
           <Link to="/homedashboard">
@@ -41,13 +54,11 @@ const NavAdmin = () => {
             <Nav.Link as={Link} to="/gestion-commentaires" onClick={(e) => handleLinkClick(e, '/gestion-commentaires')}>
               Commentaires
             </Nav.Link>
+            {/* Déconnexion */}
+            <Nav.Link onClick={handleLogout} className="logout-link">
+              Déconnexion
+            </Nav.Link>
           </Nav>
-          <Link to="/compte">
-                      <svg className="admin-connection"
-                      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path fill="#fff28e" d="M399 384.2C376.9 345.8 335.4 320 288 320l-64 0c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/>
-                      </svg>
-                    </Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
